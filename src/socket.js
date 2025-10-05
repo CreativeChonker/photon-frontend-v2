8cb797f533d3acaef15911ca96a5f6e4590334fb
Mike Erickson Munoz <150999754+CreativeChonker@users.noreply.github.com>
2025-10-06 02:14:48 +0800

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,11 @@
 import { io } from "socket.io-client";
 
 // TEMP: force polling so we bypass websocket upgrade issues
-export const socket = io("http://127.0.0.1:5001", {
+export const socket = io("https://photon-backend.onrender.com", {
   transports: ["polling"],
   reconnection: true,
   reconnectionAttempts: 10,
   reconnectionDelay: 1000,
   timeout: 25000,
 });
+
